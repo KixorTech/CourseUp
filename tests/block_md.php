501e6b297b07a29961256cb05cf6b571d8676280
Micah <micah@courseup.org>
2018-06-12 14:30:01 -0400

    Add test for PD block parsing.

---
+++ tests/block_md.php
@@ -0,0 +1,38 @@
+<?
+require_once('../PDExtension.php');
+include('../enablePublicErrors.php');
+
+$text =
+"
+*Italic* and **bold** text goes here
+- Item
+	1. Item 1
+	1. Item 2
+	1. Item 3\n";
+
+$span = '<span>'.$text."</span>\n";
+$div = '<div>'.$text."</div>\n";
+$mdiv = '<div markdown="1">'.$text."</div>\n";
+
+$pd = new Parsedown();
+print '<h2>ParseDown</h2>';
+$p = $pd->text($text); print $p;
+$p = $pd->text($span); print $p;
+$p = $pd->text($div); print $p;
+$p = $pd->text($mdiv); print $p;
+
+$pd = new ParsedownExtra();
+print '<h2>ParseDownExtra</h2>';
+$p = $pd->text($text); print $p;
+$p = $pd->text($span); print $p;
+$p = $pd->text($div); print $p;
+$p = $pd->text($mdiv); print $p;
+
+$pd = new PDExtension();
+print '<h2>PDExtension</h2>';
+$p = $pd->text($text); print $p;
+$p = $pd->text($span); print $p;
+$p = $pd->text($div); print $p;
+$p = $pd->text($mdiv); print $p;
+
+?>
