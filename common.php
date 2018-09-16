070c5be18e905e8ae7af8c0047e18763cdfb765b
Micah Taylor <micah@courseup.org>
2018-09-16 11:30:23 -0400

    Disable db since fsql version isn't php7 compatible.

--- common.php
+++ common.php
@@ -15,7 +15,7 @@ require_once("helpers.php");
 require_once("session.php");
 require_once("https.php");
 //require_once("basicAuth.php");
-require_once('db.php');
+//require_once('db.php');
 
 require_once('spyc.php');
 $configPath = getFileRoot().'/../config.yaml';
