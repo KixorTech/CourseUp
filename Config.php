33bd7cc355dafe330b0d08538e63de7df7bc22c4
Cambron Johnson <johnsocp@rose-hulman.edu>
2019-12-11 12:51:35 -0500

    changed how data is access in the config file and changed lines in comment to reflect that

--- Config.php
+++ Config.php
@@ -9,33 +9,71 @@ micah@kixortech.com
 See http://courseup.org for license information.
 */
 
-
-require_once('common.php');
-
 class Config
 {
 	private static $instance = null;
-	private $settings;
+	private $ettings;
 
 	private function __construct() 
 	{
-		$settings = array();
-		
+		$ettings = array();
+	}
+
+	public function loadSettings($configArray) {
+		$ettings = array($configArray);
+
+		foreach($ettings as $configItem) {
+
+		}
+
+		$tzName = $config['TimeZone'];
+		$tz = new DateTimeZone($tzName);
+		if(strpos($key, 'FirstQuarterDay') !== FALSE) {
+			$FirstQuarterDay = DateTime::createFromFormat('Y-m-d', $val, $tz);
+		}
+		else if(strpos($key, 'LastBeforeBreak') !== FALSE) {
+			$LastBeforeBreak = DateTime::createFromFormat('Y-m-d', $val, $tz);
+		}
+		else if(strpos($key, 'FirstAfterBreak') !== FALSE) {
+			$FirstAfterBreak = DateTime::createFromFormat('Y-m-d', $val, $tz);
+		}
+		else if(strpos($key, 'ClassOnWeekDays') !== FALSE) {
+			$ClassOnWeekDays = array();
+			$days = strtolower($val);
+			//print $val .' '. $days;
+			if( strpos($days, 'm') !== FALSE)
+				array_push($ClassOnWeekDays, 'Mon');
+			if( strpos($days, 't') !== FALSE)
+				array_push($ClassOnWeekDays, 'Tue');
+			if( strpos($days, 'w') !== FALSE)
+				array_push($ClassOnWeekDays, 'Wed');
+			if( strpos($days, 'r') !== FALSE)
+				array_push($ClassOnWeekDays, 'Thu');
+			if( strpos($days, 'f') !== FALSE)
+				array_push($ClassOnWeekDays, 'Fri');
+			//print_r($ClassOnWeekDays);
+		}
+		else if(strpos($key, 'ShowPastSessions') !== FALSE) {
+			$ShowPastSessions = $val;
+		}
+		else if(strpos($key, 'ShowFutureSessions') !== FALSE) {
+			$ShowFutureSessions = $val;
+		}
 	}
 
 	public function setConfigSettings($key, $contents) {
-		$settings[$key] = $contents;
+		$ettings[$key] = $contents;
 	}
 
 	public function getConfigSetting($key) {
-		return $settings[$key];
+		return $ettings[$key];
 	}
 
 	public static function getInstance() 
 	{
 		if (!self::$instance) 
 		{
-			self::$instance = new Calendar();
+			self::$instance = new Config();
 		}
 		return self::$instance;
 	}
--- common.php
+++ common.php
@@ -14,12 +14,14 @@ See http://courseup.org for license information.
 require_once("helpers.php");
 require_once("session.php");
 require_once("https.php");
+require_once("Config.php");
 //require_once("basicAuth.php");
 //require_once('db.php');
 
 require_once('spyc.php');
 $configPath = getFileRoot().'/../config.yaml';
 $config = spyc_load_file($configPath);
+Config::getInstance()->loadSettings($config);
 
 $online = checkOnline();
 if($online)
