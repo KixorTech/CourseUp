d928642086a4d666c7ec886603b56a4e266e4e04
penryoa <31932900+penryoa@users.noreply.github.com>
2019-12-12 15:47:57 -0500

    converted tableCreate() function in calendarMaker to PHP. mostly at least. working on it.

--- calendarMaker.php
+++ calendarMaker.php
@@ -16,9 +16,7 @@ function tableCreate() {
     $tblDiv->setAttribute('id', 'newCalendarDiv2');
 
 	$dom->appendChild($tblDiv);
-	$tblDiv->appendChild($tblDiv2);
-
-	echo $dom->saveHTML();
+	// $tblDiv->appendChild($tblDiv2);
 	
 	// appendHTML($tblDiv, '<div>these are words</div>');
 
@@ -28,38 +26,37 @@ function tableCreate() {
 
 	// echo $tblDiv;
 
-	// html = "<div "
-	// tbl.style.width = '100%';
-	// tbl.setAttribute('border', '1');
-	// var tbdy = document.createElement('tbody');
-	// for (var w = 0; w < numWeeks; w++) {
-	// 	for (var d = 0; d < numDaysAWeek; d++) {
-	// 		var tr = document.createElement('tr');
-	// 		for (var c = 0; c < 6; c++) {
-	// 			// if (i == 2 && j == 1) {
-	// 			// 	break
-	// 			// } else {
-	// 			var td = document.createElement('td');
-	// 			let text = '' // '\u0020' // adds a space?
-	// 			if (c == 0 && d == 0) {
-	// 				text = "Week " + w;
-	// 			}
-	// 			if (c == 1) {
-	// 				text = "Day " + (d + w*numDaysAWeek);
-	// 			}
+	$numWeeks = 5;
+	$numDaysAWeek = 3;
 
-	// 			td.innerHTML = text;
-	// 			// i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
-	// 			tr.appendChild(td);
-	// 			// }
-	// 		}
-	// 		tbdy.appendChild(tr);
-	// 	}
-	// }
-	// tbl.appendChild(tbdy);
+	$tblDiv->setAttribute('style', 'width:100%;');
+	$tblDiv->setAttribute('border', '1px solid #aaa;');
+	$tbdy = $dom->createElement('tbody');
+	for ($w = 0; $w < $numWeeks; $w++) {
+		for ($d = 0; $d < $numDaysAWeek; $d++) {
+			$tr = $dom->createElement('tr');
+			for ($c = 0; $c < 6; $c++) {
+				$td = $dom->createElement('td');
+				$text = ''; // '\u0020' // adds a space?
+				if ($c == 0 && $d == 0) {
+					$text = "Week " . $w;
+				}
+				if ($c == 1) {
+					$text = "Day " . ($d + $w*$numDaysAWeek);
+				}
+
+				$td->textContent = $text;
+				// i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+				$tr->appendChild($td);
+				// }
+			}
+			$tbdy->appendChild($tr);
+		}
+	}
+	$tblDiv->appendChild($tbdy);
 	// $tblDiv.append(tbl);
 	// $body->appendChild($tblDiv);
-	// $dom->saveHTML();
+	echo $dom->saveHTML();
 }
 
 
