f1e94e1fca126ad409a223737710173041002452
penryoa <31932900+penryoa@users.noreply.github.com>
2019-12-17 16:01:03 -0500

    cleaned up the calendarMaker and screen.js to make them nicer

--- calendarMaker.php
+++ calendarMaker.php
@@ -1,45 +1,25 @@
 <?php
-// <script type="text/javascript">
-
 // the following function was reference from
 // https://stackoverflow.com/questions/14643617/create-table-using-javascript
 function tableCreate() {
     $dom = new DOMDocument();
-	// $dom->loadHTML("index.php");
-
-	// $body = $dom->getElementsByTagName('body')[0];
-
-    // $dom = new DOMDocument();
-
-	// echo $body->length;
-	
 	$tblDiv = $dom->createElement('div');
-	// $tblDiv2 = $dom->createElement('div', 'these are words');
+	$tblDiv->setAttribute('id', 'newCalendarDiv');
 
-    $tblDiv->setAttribute('id', 'newCalendarDiv');
-
-	// $tblDiv->appendChild($tblDiv2);
-	
-	// appendHTML($tblDiv, '<div>these are words</div>');
-
-	// echo $tblDiv->saveHTML();
-
-    // $tblDiv->innerHTML = "hi";
-
-	// echo $tblDiv;
-
+	// Once we have the datasource figured out, the following attributes
+	// will be retrieved from there.
 	$numWeeks = 5;
 	$classDaysPerWeek = ['M','R','F'];
 	$attributeNames = ['Homework Assignments', 'Labs', 'Readings'];
 	
 
-	
+	// ===== Making the header: 
 	$tbl = $dom->createElement('table');
 	$tbl->setAttribute('style', 'width:100%;');
 	$tbl->setAttribute('border', '1px solid #aaa;');
 	$tbdy = $dom->createElement('tbody');
 
-	$headerRow = $dom->createElement('tr'); // TODO: add header column
+	$headerRow = $dom->createElement('tr');
 	$th = $dom->createElement('th');
 	$th->textContent = "Week";
 	$headerRow->appendChild($th);
@@ -55,46 +35,29 @@ function tableCreate() {
 	}
 	$tbdy->appendChild($headerRow);
 	
-
-
+	// ===== Making the table:
 	for ($w = 0; $w < $numWeeks; $w++) {
 		for ($d = 0; $d < sizeof($classDaysPerWeek); $d++) {
 			$tr = $dom->createElement('tr');
 			for ($c = 0; $c < sizeof($attributeNames)+2; $c++) {
 				$td = $dom->createElement('td');
-				$text = ''; // '\u0020' // adds a space?
+				$text = '';
 				if ($c == 0 && $d == 0) {
 					$text = "Week " . $w;
 				}
 				if ($c == 1) {
 					$text = "Day " . ($d + $w*sizeof($classDaysPerWeek));
 				}
-
 				$td->textContent = $text;
-				// i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
 				$tr->appendChild($td);
-				// }
 			}
 			$tbdy->appendChild($tr);
 		}
 	}
 	$tbl->appendChild($tbdy);
 	$tblDiv->appendChild($tbl);
-	// $body->appendChild($tblDiv);
 	$dom->appendChild($tblDiv);
 	
 	echo $dom->saveHTML();
 }
-
-
-// function appendHTML(DOMNode $parent, $source) {
-//     $tmpDoc = new DOMDocument();
-//     $tmpDoc->loadHTML($source);
-//     foreach ($tmpDoc->getElementsByTagName('body')->item(0)->childNodes as $node) {
-//         $node = $parent->ownerDocument->importNode($node, true);
-//         $parent->appendChild($node);
-//     }
-// }
-
-// </script>
 ?>
