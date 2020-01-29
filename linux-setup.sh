03ac2628d078e0c754a8b69daf5190006137db64
austinin <austinin@rose-hulman.edu>
2020-01-29 11:08:15 -0500

    Updated installation scripts to include .htaccess

--- linux-setup.sh
+++ linux-setup.sh
@@ -3,4 +3,5 @@
 rm -rf ../../../../var/www/html/*
 mkdir ../../../../var/www/html/CourseUp
 cp -r base/* ../../../../var/www/html
+cp base/.htaccess ../../../../var/www/html
 cp -r * ../../../../var/www/html/CourseUp
--- windows-install.ps1
+++ windows-install.ps1
@@ -1,1 +1,2 @@
 Copy-Item -Path "./base/*" -Destination "../" -Recurse
+Copy-Item -Path "./base/.htaccess" -Destination "../"
