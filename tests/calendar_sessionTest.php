4cadf328d590f7f991cb4731d999e4dcd110f90e
austinin <austinin@rose-hulman.edu>
2020-05-18 11:10:10 -0400

    Updated calendar test, working out config

--- tests/calendar_sessionTest.php
+++ tests/calendar_sessionTest.php
@@ -1,15 +1,16 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
-require './Calendar.php';
+require_once './Calendar.php';
 
 final class calendar_sessionTest extends TestCase
 {
 	public function testCalendar()
 	{
-		$calendar = Calendar::getInstance()->parseCalendarFile('calendarTest.md');
+		Calendar::getInstance()->parseCalendarFile('calendarTest.md');
     	$expected = 'Array ( [1] => * Sample Test due 2020-04-09.15:00 [2] => * Nothing to do [3] => * what? due +1 [4] => * last session doesn\'t show up on schedule page * nothing due +1.15:27 [5] => * see? i am hidden. also this .md breaks table view ) 1';
-    	$actual = $calendar->getCalendarString();
+    	$actual = '';
+    	$actual = Calendar::getInstance()->getCalendarString();
     	$this->assertEquals($expected, $actual);
 	}
 }
